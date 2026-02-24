1a8e90f6914fb9aadaa8677536f2279839d63b5c
Naheeria <soseno0809@gmail.com>
2026-02-25 00:11:19 +0900

    파비콘 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/session-review/', // 본인의 GitHub 저장소 이름으로 변경
 })
